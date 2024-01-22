93e6a00914114c7fee5b391e4b78e6fd5df2e36a
mosesmramba <moses.mramba@student.moringaschool.com>
2024-01-22 15:04:16 +0300

    initial commit

---
+++ App/models.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(450), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=True) 
+    bookings = db.relationship('Booking', backref='user', lazy=True)
+    reviews = db.relationship('CarReview', backref='user', lazy=True)
+
+# Car Model
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    brand = db.Column(db.String(50), nullable=False)
+    model = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String(20), nullable=False)
+    available = db.Column(db.Boolean, default=True)
+    bookings = db.relationship('Booking', backref='car', lazy=True)
+    reviews = db.relationship('CarReview', backref='car', lazy=True)
+
+# Booking Model
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    end_date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
+
+# CarReview Model
+class CarReview(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
