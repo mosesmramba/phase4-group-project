62421444c592aec873cf6cc25b17ea856e7276d5
Juliet <musyoki.juliet011@gmail.com>
2024-01-24 11:18:42 +0300

    DONE

---
+++ frontend/src/components/auth/SignInPage.jsx
@@ -0,0 +1,14 @@
+// src/components/auth/SignInPage.jsx
+import React from 'react';
+import SignInForm from './SignInForm';
+
+const SignInPage = () => {
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <SignInForm />
+    </div>
+  );
+};
+
+export default SignInPage;
