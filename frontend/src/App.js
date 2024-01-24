ef33199e4f2612eca1380f7c7254426d8833c232
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2024-01-24 22:32:20 +0300

    Create Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="*" element={<Home />} />
         </Route>
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
